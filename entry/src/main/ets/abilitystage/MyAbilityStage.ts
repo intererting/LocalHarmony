4548256e91334f38a099532fa1dcc898cc0bf96e
yuliyang <645644717@qq.com>
2024-06-11 20:34:25 +0800

    init commit

--- entry/src/main/ets/abilitystage/MyAbilityStage.ts
+++ entry/src/main/ets/abilitystage/MyAbilityStage.ts
@@ -1,5 +1,6 @@
 import AbilityStage from '@ohos.app.ability.AbilityStage';
 import Want from '@ohos.app.ability.Want';
+import hilog from '@ohos.hilog';
 
 export default class MyAbilityStage extends AbilityStage {
   onCreate() {
@@ -7,8 +8,9 @@ export default class MyAbilityStage extends AbilityStage {
   }
 
   onAcceptWant(want: Want): string {
-    if (want.abilityName == 'FunctionAbility') {
-      return `specified_${want.parameters?.instanceKey ?? ''}`
+    if (want.abilityName == 'EntryAbility') {
+      hilog.debug(0x0001, 'MyAbilityStageTag', "onAcceptWant")
+      return `specified_entry ${Math.random()}'}`
     }
     return ''
   }
---
+++ entry/src/main/ets/entryability/specified/SeondAbility.ts
@@ -0,0 +1,14 @@
+import UIAbility from '@ohos.app.ability.UIAbility';
+import Want from '@ohos.app.ability.Want';
+import AbilityConstant from '@ohos.app.ability.AbilityConstant';
+import window from '@ohos.window';
+
+export default class SecondAbility extends UIAbility {
+  onCreate(want: Want, param: AbilityConstant.LaunchParam) {
+
+  }
+
+  onWindowStageCreate(windowStage: window.WindowStage) {
+    windowStage.loadContent("pages/specified/SecondPage")
+  }
+}
